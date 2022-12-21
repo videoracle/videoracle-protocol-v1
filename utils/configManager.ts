957cc69b10294c85d5bced301238b5e56a789f43
mattiapomelli <mattiapomelli@gmail.com>
2022-12-21 22:08:41 +0100

    Add config manager

---
+++ utils/configManager.ts
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+
+export enum ConfigProperty {
+  SimpleERC20 = "simpleERC20Address",
+  VideOracle = "videOracleAddress",
+}
+
+const getFilename = (network: string) =>
+  path.resolve(__dirname, `../configs/${network}.json`);
+
+const loadJSON = (network: string) => {
+  const filename = getFilename(network);
+  return fs.existsSync(filename) ? fs.readFileSync(filename).toString() : "{}";
+};
+
+const saveJSON = (network: string, json = "") => {
+  const filename = getFilename(network);
+  return fs.writeFileSync(filename, JSON.stringify(json, null, 2));
+};
+
+export const get = (network: string, property: ConfigProperty): string => {
+  const obj = JSON.parse(loadJSON(network));
+  return obj[property] || "Not found";
+};
+
+export const set = (
+  network: string,
+  property: ConfigProperty,
+  value: string
+) => {
+  const obj = JSON.parse(loadJSON(network) || "{}");
+  obj[property] = value;
+  saveJSON(network, obj);
+};
+
+export const remove = (network: string, property: ConfigProperty) => {
+  const obj = JSON.parse(loadJSON(network) || "{}");
+  delete obj[property];
+  saveJSON(network, obj);
+};
