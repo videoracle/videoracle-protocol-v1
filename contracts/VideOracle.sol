c403e9c67418b9b4f2a949f418b461a9072f2595
Giacomo Brunetti <25010047+thundering-silence@users.noreply.github.com>
2023-01-06 14:59:15 +0100

    fee collection via smart contract

--- contracts/VideOracle.sol
+++ contracts/VideOracle.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {Multicall} from "@openzeppelin/contracts/utils/Multicall.sol";
 
-contract VideOracle {
-
-    uint256 constant TOTAL_VOTES = 5;
-
-    enum RequestStatus { ACTIVE, VOTING, CLOSED }
+contract VideOracle is Ownable2Step, ReentrancyGuard, Multicall {
+    enum RequestStatus {
+        ACTIVE,
+        VOTING,
+        CLOSED
+    }
     struct Request {
         uint256 endTime;
         uint256 reward;
@@ -15,30 +19,53 @@ contract VideOracle {
         address requester;
         string requestUri;
     }
-
-    mapping(uint256 => Request) public requests;
-    uint256 public requestsCount;
-
     struct Proof {
         uint256 tokenId;
         address payable verifier;
     }
 
-    mapping(uint256 => mapping(uint256 => Proof)) public proofsByRequest;
-    mapping(uint256 => uint256) public proofsCount4Request;
-    mapping(uint256 => mapping(uint256 => uint256)) public pointsForProof4Request;
-    mapping(uint256 => mapping(uint256 => bool)) public claimedProof4Request;
-
-    event NewRequest(address indexed src, uint256 requestId, uint256 endTime, uint256 reward,  string requestUri);
-    event NewProof(address indexed src, uint256 indexed requestId, uint256 proofId, uint256 tokenId);
+    event NewRequest(
+        address indexed src,
+        uint256 requestId,
+        uint256 endTime,
+        uint256 reward,
+        string requestUri
+    );
+    event NewProof(
+        address indexed src,
+        uint256 indexed requestId,
+        uint256 proofId,
+        uint256 tokenId
+    );
     event RequestVotingOpened(address indexed src, uint256 indexed requestId);
     event RequestClosed(address indexed src, uint256 indexed requestId);
 
-    function createRequest(uint256 time2proof, uint256 reward, string calldata requestURI) public payable returns(uint256 requestId) {
-        require(msg.value >= reward, 'value sent not enough');
-        uint256 endTime = block.timestamp + time2proof;
+    uint256 constant TOTAL_VOTES = 5;
+    uint256 private _feeBPS = 300; // 100% = 10000
+    address private _feeCollector;
+
+    mapping(uint256 => Request) public requests;
+    uint256 public requestsCount;
+    mapping(uint256 => mapping(uint256 => Proof)) public proofsByRequest;
+    mapping(uint256 => uint256) public proofsCount4Request;
+    mapping(uint256 => mapping(uint256 => uint256))
+        public pointsForProof4Request;
+    mapping(uint256 => mapping(uint256 => bool)) public claimedProof4Request;
+
+    constructor(address feeCollector_) {
+        _feeCollector = feeCollector_;
+    }
+
+    function createRequest(
+        uint256 time2proof,
+        uint256 reward,
+        string calldata requestURI
+    ) public payable nonReentrant returns (uint256 requestId) {
+        uint256 minIn = (reward * (1e5 + _feeBPS)) / 1e5;
+        require(msg.value >= minIn, "value sent not enough");
 
         requestId = requestsCount++;
+        uint256 endTime = block.timestamp + time2proof;
         requests[requestId] = Request({
             endTime: endTime,
             reward: reward,
@@ -47,21 +74,32 @@ contract VideOracle {
             requestUri: requestURI
         });
 
-        if (msg.value > reward) {
-            Address.sendValue(payable(msg.sender),  msg.value - reward);
+        // Send fee to collector
+        Address.sendValue(payable(_feeCollector), (reward * _feeBPS) / 1e5);
+        if (msg.value > minIn) {
+            Address.sendValue(payable(msg.sender), msg.value - minIn);
         }
 
         emit NewRequest(msg.sender, requestId, endTime, reward, requestURI);
     }
 
-    function submitProof(uint256 requestId, uint256 tokenId) public returns(uint256 proofId) {
-        require(requestId < requestsCount, 'request does not exist');
+    function submitProof(uint256 requestId, uint256 tokenId)
+        public
+        returns (uint256 proofId)
+    {
+        require(requestId < requestsCount, "request does not exist");
 
         Request memory request = requests[requestId];
 
-        require(request.requester != msg.sender, 'you cannot proof your own request');
+        require(
+            request.requester != msg.sender,
+            "you cannot proof your own request"
+        );
 
-        require(request.status == RequestStatus.ACTIVE, 'request not in active state');
+        require(
+            request.status == RequestStatus.ACTIVE,
+            "request not in active state"
+        );
 
         proofId = proofsCount4Request[requestId]++;
 
@@ -73,28 +111,44 @@ contract VideOracle {
         emit NewProof(msg.sender, requestId, proofId, tokenId);
     }
 
-    function voteProofs(uint256 requestId, uint256[] calldata proofsIds, uint256[] calldata points) public {
-        require(proofsIds.length == points.length, 'check proofsIds and points');
+    function voteProofs(
+        uint256 requestId,
+        uint256[] calldata proofsIds,
+        uint256[] calldata points
+    ) public {
+        require(
+            proofsIds.length == points.length,
+            "check proofsIds and points"
+        );
 
         Request storage request = requests[requestId];
 
-        require(request.requester == msg.sender, 'only proofer can vote their own requests');
+        require(
+            request.requester == msg.sender,
+            "only proofer can vote their own requests"
+        );
 
-        if (request.status == RequestStatus.ACTIVE && request.endTime <= block.timestamp) {
+        if (
+            request.status == RequestStatus.ACTIVE &&
+            request.endTime <= block.timestamp
+        ) {
             request.status = RequestStatus.VOTING;
             emit RequestVotingOpened(request.requester, requestId);
         }
 
-        require(request.status == RequestStatus.VOTING, 'request not in voting state');
+        require(
+            request.status == RequestStatus.VOTING,
+            "request not in voting state"
+        );
 
         uint256 totalVotes = 0;
-        for(uint256 i = 0; i < proofsIds.length; i++) {
+        for (uint256 i = 0; i < proofsIds.length; i++) {
             uint256 proofPoints = points[i];
             totalVotes += proofPoints;
             pointsForProof4Request[requestId][proofsIds[i]] = proofPoints;
         }
 
-        require(totalVotes <= TOTAL_VOTES, 'too many votes');
+        require(totalVotes <= TOTAL_VOTES, "too many votes");
 
         request.status = RequestStatus.CLOSED;
         emit RequestClosed(request.requester, requestId);
@@ -102,13 +156,17 @@ contract VideOracle {
 
     function claim(uint256 requestId, uint256 proofId) public {
         uint256 points = pointsForProof4Request[requestId][proofId];
-        require(points > 0, 'no points to your request');
+        require(points > 0, "no points to your request");
 
-        require(claimedProof4Request[requestId][proofId] == false, 'already claimed');
+        require(
+            claimedProof4Request[requestId][proofId] == false,
+            "already claimed"
+        );
 
         claimedProof4Request[requestId][proofId] = true;
 
-        uint256 proofReward = requests[requestId].reward * points / TOTAL_VOTES;
+        uint256 proofReward = (requests[requestId].reward * points) /
+            TOTAL_VOTES;
 
         Address.sendValue(
             (proofsByRequest[requestId][proofId]).verifier,
