f9965a5ec4021b431b1ca65bde04eb56328fb7fe
codethazine <dario.finocchiaro01@gmail.com>
2023-01-17 18:33:55 +0100

    deployed contract added on v1

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,31 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
-import "@typechain/hardhat";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
-dotenv.config();
-
-const config: HardhatUserConfig = {
-    solidity: "0.8.17",
-};
-
-export default config;
+module.exports = {
+  defaultNetwork: "polygon",
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    polygon: {
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    version: "0.8.13",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
