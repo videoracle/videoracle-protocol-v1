497104866c3126a9384876005d8118994f20c55d
mattiapomelli <mattiapomelli@gmail.com>
2023-01-19 10:18:57 +0000

    FIx config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,31 +1,37 @@
-require('dotenv').config();
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-etherscan");
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
 
-module.exports = {
-  defaultNetwork: "polygon",
+dotenv.config();
+
+if (!process.env.PRIVATE_KEY) {
+  throw new Error("Please set your PRIVATE_KEY in a .env file");
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-    },
     polygon_mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [process.env.PRIVATE_KEY]
+      accounts: [process.env.PRIVATE_KEY],
     },
     polygon: {
       url: "https://rpc-mainnet.maticvigil.com",
-      accounts: [process.env.PRIVATE_KEY]
-    }
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
   etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY
+    apiKey: process.env.POLYGONSCAN_API_KEY,
   },
   solidity: {
     version: "0.8.13",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
-}
+};
+
+export default config;
